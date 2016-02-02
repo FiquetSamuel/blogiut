1b54c20c9d4c61887d382ba0f92767a241a570dd
FiquetSamuel <fiquet.samuel@outlook.fr>
2016-02-02 18:39:10 +0100

    Correction mise en forme du code vignette.jpg.php

--- vignette.jpg.php
+++ vignette.jpg.php
@@ -1,25 +1,25 @@
 <?php
 
-include('includes/connexion.inc.php');
-header("Content-type: image/jpg");
+	include('includes/connexion.inc.php');
+	header("Content-type: image/jpg");
 
-//Calcul ratio et taille voulue.
-$id = $_GET['id'];
-$cheminImg = "data/images/$id.jpg";
-list($orgLarg, $orgHaut) = getimagesize($cheminImg);
+	//Calcul ratio et taille voulue.
+	$id = $_GET['id'];
+	$cheminImg = "data/images/$id.jpg";
+	list($orgLarg, $orgHaut) = getimagesize($cheminImg);
 
-$largVoulue = 200;
-$ratio = $orgLarg/$largVoulue;
+	$largVoulue = 200;
+	$ratio = $orgLarg/$largVoulue;
 
-$newLarg = (int) $orgLarg/$ratio;
-$newHaut = (int) $orgHaut/$ratio;
+	$newLarg = (int) $orgLarg/$ratio;
+	$newHaut = (int) $orgHaut/$ratio;
 
-//Création image
-$imgCreat = imagecreatetruecolor($newLarg, $newHaut);
-$myImg = imagecreatefromjpeg($cheminImg);
-imagecopyresampled($imgCreat, $myImg, 0, 0, 0, 0, $newLarg, floor($newHaut), $orgLarg, $orgHaut);
+	//Création image
+	$imgCreat = imagecreatetruecolor($newLarg, $newHaut);
+	$myImg = imagecreatefromjpeg($cheminImg);
+	imagecopyresampled($imgCreat, $myImg, 0, 0, 0, 0, $newLarg, floor($newHaut), $orgLarg, $orgHaut);
 
-//Affichage
-imagejpeg($imgCreat, null, 100);
+	//Affichage
+	imagejpeg($imgCreat, null, 100);
 
 ?>
