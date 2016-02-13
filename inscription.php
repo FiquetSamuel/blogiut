ed75c0646dac768f5305249c1b4cca3e03a00b63
FiquetSamuel <fiquet.samuel@outlook.fr>
2016-02-13 01:07:33 +0100

    Vérification que tout les champs soient rempli est finie.

--- inscription.php
+++ inscription.php
@@ -4,10 +4,10 @@
 	include('includes/verif_util.inc.php');
 ?>
 
-	<form action="inscription.php" method="post">
+<form action="inscription.php" method="post">
 		<div class="clearfix">
 			<label for="nom">Nom:</label>
-			<input name="titre" size="255" type="text">
+			<input name="nom" size="255" type="text">
 		</div>
 		<div class="clearfix">
 			<label for="prenom">Prenom:</label>
@@ -25,10 +25,33 @@
 			<label for="mdpCheck">Confirmation mot de passe:</label>
 			<input name="mdpCheck" size="255" type="password">
 		</div>
-		<input class="btn btn-primary" id="envoi" type="submit" value="Enregistrer">
+		<input class="btn btn-primary" name="envoi" type="submit" value="Enregistrer">
 	</form>
-		
 
 <?php
+	//Redirige l'utilisateur sur l'accueil si il est déjà connecté.
+	if($connect == true){
+		header('Location:index.php');
+	}else{
+		if(isset($_POST['envoi'])){
+			//On vérifie que tout les champs sont rempli.
+			$isFilled = array('nom', 'prenom', 'email', 'mdp', 'mdpCheck');
+			$error = false;
+			foreach($isFilled as $check){
+				if(empty($_POST[$check])){
+					$error = true;
+				}
+			}
+			
+			//Si true alors un champs n'est pas rempli.
+			//Sinon, on vérifie la validité des données passé.
+			if($error == true){
+				echo '<div class="alert alert-warning">
+						<strong>Tout les champs doivent être rempli.</strong>
+				</div>';
+			}
+		}
+	}
+
 	include('includes/footer.inc.php');
 ?>
